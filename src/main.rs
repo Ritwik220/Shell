3ac48e9f6f1a9b002a344c6a24afb0ebb0e6e7eb
Ritwik <ritwikyadav35@gmail.com>
2025-09-14 00:02:27 +0530

    Implemented shell built-ins

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::path::Path;
 use std::io::{stdout, Write};
 
 fn main() {
@@ -12,10 +13,23 @@ fn main() {
         let command = parts.next().unwrap();
         let args = parts;
 
-        let mut child = Command::new(command)
-            .args(args)
-            .spawn()
-            .unwrap();
-        child.wait();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir); 
+                if let Err(e) = std::env::set_current_dir(&root) {
+                    eprintln!("cd: {}: {}", e, new_dir);
+                }
+            }
+            command => {
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
+
+                child.wait();
+            }
+        }
+
     }
 }
