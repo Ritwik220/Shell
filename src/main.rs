18bc228e5832ee3dbb7acd0fb084afabf1efdf81
Ritwik <ritwikyadav35@gmail.com>
2025-09-13 23:32:51 +0530

    Can now handle arguments

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ fn main() {
 
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-        let command = input.trim();
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
         let mut child = Command::new(command)
+            .args(args)
             .spawn()
             .unwrap();
-        child.wait().unwrap();
+        child.wait();
     }
 }
