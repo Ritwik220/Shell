d78b8324b1e132908830060463fecad6aff0c7bf
Ritwik <ritwikyadav35@gmail.com>
2025-09-15 17:48:05 +0530

    Fixed a few errors.

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,35 @@ fn main() {
 
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command :Option<&str> = None;
+        while let Some(command) = commands.next() {
+            let mut parts = command.split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
 
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir); 
-                if let Err(e) = std::env::set_current_dir(&root) {
-                    eprintln!("cd: {}: {}", e, new_dir);
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = std::env::set_current_dir(&root) {
+                        eprintln!("cd: {}: {}", e, new_dir);
+                    }
+                },
+                "exit" => return,
+                command => {
+                    let mut child = Command::new(command)
+                        .args(args)
+                        .spawn();
+
+                    match child {
+                        Ok(mut child) => { child.wait(); },
+                        Err(e) => { eprintln!("{}", e); },
+                    };
                 }
-            },
-            "exit" => return,
-            command => {
-                let mut child = Command::new(command)
-                    .args(args)
-                    .spawn();
 
-                match child {
-                    Ok(mut child) => { child.wait(); },
-                    Err(e) => { eprintln!("{}", e); },
-                };
             }
-        };
+        }
 
     }
 }
